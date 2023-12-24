2db36e528eacbde5d7716c4339d4b91c7ea56868
James Gonzales <jgonz900@gmail.com>
2023-12-23 23:53:30 -0500

    Add date of posting to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from '../posts/PostAuthor'
+import { TimeAgo } from '../posts/TimeAgo'
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
 
-    const renderedPosts = posts.map(post => (
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+    const renderedPosts = orderedPosts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
             <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className="button muted-button">
                 View Post
